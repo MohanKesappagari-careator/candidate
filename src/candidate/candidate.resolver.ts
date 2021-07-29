a9397e32ba176cc35401fbf35fe307b071407d81
mohankesappagari <mohan.k@careator.com>
2021-07-29 12:25:55 +0530

    candidate module

---
+++ src/candidate/candidate.resolver.ts
@@ -0,0 +1,42 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { CandidateService } from './candidate.service';
+import { Candidate } from './entities/candidate.entity';
+import { CreateCandidateInput } from './dto/create-candidate.input';
+import { UpdateCandidateInput } from './dto/update-candidate.input';
+
+@Resolver(() => Candidate)
+export class CandidateResolver {
+  constructor(private readonly candidateService: CandidateService) {}
+
+  @Mutation(() => Candidate)
+  createCandidate(
+    @Args('createCandidateInput') createCandidateInput: CreateCandidateInput,
+  ) {
+    return this.candidateService.create(createCandidateInput);
+  }
+
+  @Query(() => [Candidate], { name: 'Allcandidate' })
+  findAll() {
+    return this.candidateService.findAll();
+  }
+
+  @Query(() => Candidate, { name: 'candidate' })
+  findOne(@Args('id') id: string) {
+    return this.candidateService.findOne(id);
+  }
+
+  @Mutation(() => Candidate)
+  updateCandidate(
+    @Args('updateCandidateInput') updateCandidateInput: UpdateCandidateInput,
+  ) {
+    return this.candidateService.update(
+      updateCandidateInput.id,
+      updateCandidateInput,
+    );
+  }
+
+  @Mutation(() => Candidate)
+  removeCandidate(@Args('id') id: string) {
+    return this.candidateService.remove(id);
+  }
+}
---
+++ src/candidate/candidate.service.ts
@@ -0,0 +1,70 @@
+import { HttpException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCandidateInput } from './dto/create-candidate.input';
+import { UpdateCandidateInput } from './dto/update-candidate.input';
+import { Candidate } from './entities/candidate.entity';
+
+@Injectable()
+export class CandidateService {
+  constructor(
+    @InjectRepository(Candidate)
+    private candidateRepository: Repository<Candidate>,
+  ) {}
+  async create(createCandidateInput: CreateCandidateInput) {
+    const { name, email, dateOfBirth } = createCandidateInput;
+    var year = dateOfBirth.getFullYear();
+    var month = dateOfBirth.getMonth();
+    var day = dateOfBirth.getDate();
+    let today = new Date();
+    var d = today.getFullYear();
+    let age: number = d - year;
+    if (
+      today.getMonth() < month ||
+      (today.getMonth() == month && today.getDate() < day)
+    ) {
+      age--;
+    }
+
+    return await this.candidateRepository.save({
+      name: name,
+      email: email,
+      dateOfBirth: dateOfBirth,
+      age: age,
+    });
+  }
+
+  async findAll() {
+    return await this.candidateRepository.find();
+  }
+
+  async findOne(id: string) {
+    return await this.candidateRepository.findOne(id);
+  }
+
+  async update(id: string, updateCandidateInput: UpdateCandidateInput) {
+    const candidate = await this.candidateRepository.create(
+      updateCandidateInput,
+    );
+    const update = await this.candidateRepository.update(
+      { id: updateCandidateInput.id },
+      candidate,
+    );
+    if (update.affected === 1) {
+      return candidate;
+    }
+  }
+
+  async remove(id: string) {
+    const candidate = await this.candidateRepository.findOne(id);
+    if (!candidate) {
+      throw new HttpException('Not Found', 404);
+    }
+    const del = await this.candidateRepository.delete(id);
+    if (del.affected === 1) {
+      return candidate;
+    }
+  }
+}
